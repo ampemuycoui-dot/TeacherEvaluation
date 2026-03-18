0006ebebc7024410ebad2040114941af513c582d
ampemuycoui-dot <ampe.muyco.ui@phinmaed.com>
2026-03-18 12:36:45 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", function(e) {
   e.waitUntil(
-    caches.open("todo-list-cache").then(function(cache) {
+    caches.open("teacher-eval-cache").then(function(cache) {
       return cache.addAll(["index.html"]);
     })
   );
